8446fe4b459ee306efbfba02c349e71d75b7aaa1
Swapnil Musale <swapnilmusale19@gmail.com>
2023-11-28 22:23:20 +0530

    Initial commit

---
+++ composeApp/src/jsMain/kotlin/com/devx/cmp/theme/Theme.js.kt
@@ -0,0 +1,7 @@
+package com.devx.cmp.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal actual fun SystemAppearance(isDark: Boolean) {
+}
---
+++ composeApp/src/jvmMain/kotlin/com/devx/cmp/App.jvm.kt
@@ -0,0 +1,9 @@
+package com.devx.cmp
+
+import java.awt.Desktop
+import java.net.URI
+
+internal actual fun openUrl(url: String?) {
+    val uri = url?.let { URI.create(it) } ?: return
+    Desktop.getDesktop().browse(uri)
+}
